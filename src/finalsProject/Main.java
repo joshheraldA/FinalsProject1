d6b831862460701bbec4a5249a094c89567ab460
joshheraldA <joshherald19@gmail.com>
2025-12-12 19:08:02 +0800

    Seconnd commit hopefully

--- src/finalsProject/Main.java
+++ src/finalsProject/Main.java
@@ -3,5 +3,6 @@ package finalsProject;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is Josh Herald Y. Abelardo");
+		System.out.println("HELLO");
 	}
 }
