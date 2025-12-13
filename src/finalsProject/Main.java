77a627c4893ce385c1e117f3df953f42a66176ed
Mark Steven Tsai V. Go <marksteventsaigo5@gmail.com>
2025-12-13 13:05:02 +0800

    This is marks first commit

--- src/finalsProject/Main.java
+++ src/finalsProject/Main.java
@@ -5,5 +5,7 @@ public class Main {
 		System.out.println("This is Josh Herald Y. Abelardo");
 		System.out.println("HELLO");
 		System.out.println("This is Josh branch");
+		System.out.println("This is mark branch");
+
 	}
 }
